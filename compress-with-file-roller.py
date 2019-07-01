dca39015aa27e47f10a40d103ae8f69fef0919b9
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2019-06-30 20:09:02 -0400

    update license

--- compress-with-file-roller.py
+++ compress-with-file-roller.py
@@ -2,20 +2,12 @@
 
 "Compress with File-Roller" Nautilus extension
 
-Copyright (c) 2018 Carlos Cesar Caballero Diaz.
+"THE SODA-WARE LICENSE":
+As long as you retain this notice you can do whatever you want 
+with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a soda in return. 
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+Carlos Cesar Caballero Diaz.
 
 """
 
